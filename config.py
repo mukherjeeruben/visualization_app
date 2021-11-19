fd66fd65b11dac946908dd4e981b629e5e632c1f
mukherjeeruben <rubenmukherjee@outlook.com>
2021-11-19 18:21:50 +0000

    added url for master data

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 debug = False  # Push False on Server
+location_master_data = '''https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv'''
 
 query_year = {'2019': '2upf-qytp',
               '2020': 'kxp8-n2sj'}
 
-chunk_set = 1000000
+chunk_set = 10
